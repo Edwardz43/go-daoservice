b8c66a5cc3c02b1377ba43e976e69bf453bcc03c
EdLo <b93303015@gmail.com>
2019-04-06 18:44:50 +0800

    Add pprof benchmark setup

--- test/service_test.go
+++ test/service_test.go
@@ -2,6 +2,11 @@ package test
 
 import (
 	"database/sql"
+	"flag"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
 	"testing"
 	"time"
 
@@ -13,6 +18,9 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func TestDriverName(t *testing.T) {
 	if config.DriverName == "mysql" {
@@ -60,16 +68,66 @@ func TestQuery(t *testing.T) {
 }
 
 func BenchmarkQuery(b *testing.B) {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	for i := 0; i < b.N; i++ {
 		dao.Query("SELECT * FROM demo.users;")
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close()
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
 	}
 }
 
 func BenchmarkExecute(b *testing.B) {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	stmt1 := "INSERT INTO demo.users (name, email, password, created_at) VALUES(?, ?, ?, ?);"
 	for i := 0; i < b.N; i++ {
 		dao.Execute(stmt1, faker.Username(), faker.Email(), faker.Password(), time.Now())
 	}
 
 	dao.Execute("TRUNCATE TABLE demo.users;")
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close()
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 }
